aa358453717266c6b3c201963d568ff196d679d7
Charlie Flux <mail@charlieflux.com>
2025-04-28 12:07:22 -0700

    Refreshed gitignore

---
+++ BoxBrawls/Assets/Scripts/powespawner.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class powespawner : MonoBehaviour
+{
+    // This script handles the spawning of powerups in the game.
+    // It randomly spawns powerups at specified intervals and ensures they are not spawned when the player or enemy is inactive.
+    public GameObject[] myobject; 
+    float timePassed = 0f; 
+
+    public GameObject player; 
+    public GameObject enemy; 
+
+    private bool shouldSpawnPowerups = true; 
+
+    void Update()
+    {
+        if (shouldSpawnPowerups)
+        {
+            timePassed += Time.deltaTime; 
+
+            if (timePassed > 1.0f)
+            {
+                int randomIndex = Random.Range(0, myobject.Length); 
+                Vector3 randomspawnposition = new Vector3(Random.Range(-9, 9), 1, Random.Range(-9, 9)); // Random spawn location
+
+                Instantiate(myobject[randomIndex], randomspawnposition, Quaternion.identity); // Spawn it
+
+                timePassed = 0f; 
+            }
+        }
+
+        
+        if (!player.activeSelf || !enemy.activeSelf)
+        {
+            shouldSpawnPowerups = false;
+        }
+    }
+}
