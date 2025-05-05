0b06dff135e6f62d79a042a68dfd6088f61d06ab
Charlie Flux <mail@charlieflux.com>
2025-05-05 14:33:02 -0700

    Added first draft of stadium pulse

---
+++ BoxBrawls/Assets/Scripts/StadiumPulseController.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StadiumPulseController : MonoBehaviour
+{
+    public playercontroller playerScript;
+    public enemyscript opponentScript;
+
+    public Image fillImage;
+    public Color lowColor = Color.green;
+    public Color midColor = Color.yellow;
+    public Color highColor = Color.red;
+
+    void Update()
+    {
+        if (playerScript != null && opponentScript != null)
+        {
+            float playerRatio = playerScript.health / playerScript.maxhealth;
+            float opponentRatio = opponentScript.health / opponentScript.maxhealth;
+            float minRatio = Mathf.Min(playerRatio, opponentRatio);
+
+            float intensity = 1f - minRatio;
+
+            fillImage.fillAmount = intensity;
+
+            if (intensity < 0.5f)
+            {
+                float t = intensity / 0.5f;
+                fillImage.color = Color.Lerp(lowColor, midColor, t);
+            }
+            else
+            {
+                float t = (intensity - 0.5f) / 0.5f;
+                fillImage.color = Color.Lerp(midColor, highColor, t);
+            }
+        }
+    }
+}
